9e1d26370d3617bf957c367d0af0a325610fd2ce
Omer Farook <49291618+omerfarookbe@users.noreply.github.com>
2024-04-04 12:26:04 -0400

    inital commit

---
+++ frmProcess.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ImportCSV
+{
+    partial class frmImport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dialogFileImport = new System.Windows.Forms.OpenFileDialog();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dialogFileImport
+            // 
+            this.dialogFileImport.FileOk += new System.ComponentModel.CancelEventHandler(this.dialogFileImport_FileOk);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(241, 75);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(170, 41);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblProgress.Location = new System.Drawing.Point(0, 196);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(646, 20);
+            this.lblProgress.TabIndex = 1;
+            this.lblProgress.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmImport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 216);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.btnImport);
+            this.Font = new System.Drawing.Font("Arial", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmImport";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Import CSV to Warehouse";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmImport_FormClosing);
+            this.Load += new System.EventHandler(this.frmImport_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog dialogFileImport;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Label lblProgress;
+    }
+}
+
---
+++ frmProcess.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImportCSV
+{
+    public partial class frmImport : Form
+    {
+        public frmImport()
+        {
+            InitializeComponent();
+        }
+
+        private void frmImport_Load(object sender, EventArgs e)
+        {
+            string initialDirectory = "C:\\"; //Figure out an initial directory from somewhere
+
+            dialogFileImport.InitialDirectory = !Directory.Exists(initialDirectory)
+                                                   ? Path.GetPathRoot(Environment.SystemDirectory)
+                                                   : initialDirectory;
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            dialogFileImport.ShowDialog();
+
+        }
+
+        private void dialogFileImport_FileOk(object sender, CancelEventArgs e)
+        {
+            btnImport.Enabled = false;
+            Thread thread = new Thread(new ThreadStart(GetDataTabletFromCSVFile));
+            thread.Start();
+        }
+
+        private void GetDataTabletFromCSVFile()
+        {
+            DataTable csvData = new DataTable();
+            try
+            {
+                DataTable tblcsv = new DataTable();
+                tblcsv.Columns.Add("column1");
+                tblcsv.Columns.Add("column2");
+                tblcsv.Columns.Add("column3");
+                tblcsv.Columns.Add("column4");
+                string CSVFilePath = Path.GetFullPath(dialogFileImport.FileName);
+                string ReadCSV = File.ReadAllText(CSVFilePath);
+                int i = 0;
+                foreach (string csvRow in ReadCSV.Split('\n'))
+                {
+                    if (i == 0)
+                    { i++; continue; }
+
+                    if (!string.IsNullOrEmpty(csvRow))
+                    {
+                        tblcsv.Rows.Add();
+                        int count = 0;
+                        foreach (string FileRec in csvRow.Split(','))
+                        {
+                            tblcsv.Rows[tblcsv.Rows.Count - 1][count] = FileRec;
+                            count++;
+                        }
+                    }
+                    i++;
+                    lblProgress.Invoke((MethodInvoker)(() => lblProgress.Text = "Importing from CSV " + i));
+                }
+                InsertCSVRecords(tblcsv);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                btnImport.Enabled = true;
+                lblProgress.Text = "";
+            }
+        }
+
+        private void InsertCSVRecords(DataTable csvdt)
+        {
+            string sqlconn = "connectionstring to sql server";
+	    string table_name = "table name in sql server";
+
+            SqlConnection con = new SqlConnection(sqlconn);
+            SqlCommand cmd = new SqlCommand($"truncate table {table_name}", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            using (SqlConnection destinationCon = new SqlConnection(sqlconn))
+            {
+                using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(destinationCon))
+                {
+                    sqlBulkCopy.BulkCopyTimeout = 3600;
+                    sqlBulkCopy.DestinationTableName = table_name;
+                    sqlBulkCopy.BatchSize = 10000;
+                    sqlBulkCopy.NotifyAfter = 5000;
+                    sqlBulkCopy.SqlRowsCopied += (s, e) =>
+                    {
+                        lblProgress.Invoke((MethodInvoker)(() => lblProgress.Text = "Uploading to SQL " + e.RowsCopied));
+                    };
+                    destinationCon.Open();
+                    sqlBulkCopy.WriteToServer(csvdt);
+                }
+            }
+        }
+
+        private void frmImport_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+}
